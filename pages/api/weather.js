51d38d5fcb6d329f0a1d3afa170ac74ac52a2811
Jan Vlnas <jan.vlnas@superface.ai>
2022-02-16 22:48:40 +0100

    Add weather API endpoint

---
+++ pages/api/weather.js
@@ -0,0 +1,26 @@
+import { SuperfaceClient } from '@superfaceai/one-sdk';
+
+export default async function handler(req, res) {
+  try {
+    const sdk = new SuperfaceClient();
+    const profile = await sdk.getProfile('weather/current-city');
+
+    const result = await profile.getUseCase('GetCurrentWeatherInCity').perform({
+      city: 'Prague, Czech Republic',
+      units: 'C',
+    });
+
+    result.match(
+      (data) => {
+        res.status(200).json(data);
+      },
+      (error) => {
+        res.status(500).json({ message: 'Perform error', error });
+      }
+    );
+  } catch (error) {
+    res
+      .status(500)
+      .json({ message: 'Caught exception outside of perform', error });
+  }
+}
