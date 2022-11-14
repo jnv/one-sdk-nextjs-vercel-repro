af9283b6fbc1b950aa57f0cbcbcded1b6a6a8a73
Jan Vlnas <jan.vlnas@superface.ai>
2022-11-14 13:08:28 +0100

    Fix nft workaround

--- sdk.js
+++ sdk.js
@@ -1,13 +1,16 @@
 import { SuperfaceClient } from '@superfaceai/one-sdk';
+import { readdir } from 'fs';
+import { resolve } from 'path';
 
 // Trick @vercel/nft to include the whole superface/ dir
 function noop() {
   try {
-    fs.readdir(resolve(__dirname, '..', '..', 'superface'));
+    readdir(resolve(__dirname, 'superface'));
   } catch (e) {}
 }
 
 let sdk;
+
 function getSdk() {
   if (sdk) {
     return sdk;
